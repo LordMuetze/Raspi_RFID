37f1b724275f89e757917fd0f637e2ba4c3299c8
Lord Muetze <valentin-herrmann@gmx.net>
2018-03-13 18:45:26 +0100

    Standard commit

--- Test.py
+++ Test.py
@@ -1,5 +1,10 @@
 import RFID
 
 while True:
-    print(RFID.check())
+    zugang = RFID.check()
+    if zugang:
+        print("Herein spaziert!")
+    if not zugang:
+        print("Zugang verweigert")
+    sleep(0.5)
 
