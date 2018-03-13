a1dd37e3f547079575c2a3de99c8fe78cefa1ac5
Lord Muetze <valentin-herrmann@gmx.net>
2018-03-13 18:06:21 +0100

    Standard commit

--- Test.py
+++ Test.py
@@ -18,25 +18,24 @@ while True:
     
 
     # Scan for cards    
-    #(status,TagType) = Kartenleser.MFRC522_Request(Kartenleser.PICC_REQIDL)
+    (status,TagType) = Kartenleser.MFRC522_Request(Kartenleser.PICC_REQIDL)
 
     # If a card is found
-    #if status == Kartenleser.MI_OK:
+    if status == Kartenleser.MI_OK:
         # Get the UID of the card
-        #(status,uid) = Kartenleser.MFRC522_Anticoll()
+        (status,uid) = Kartenleser.MFRC522_Anticoll()
         # This is the default key for authentication
         key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
         # Select the scanned tag
-        #Kartenleser.MFRC522_SelectTag(uid)
+        Kartenleser.MFRC522_SelectTag(uid)
         # Authenticate
-        #status = Kartenleser.MFRC522_Auth(Kartenleser.PICC_AUTHENT1A, 8, key, uid)
-
-        #print("\n"+str(status))
+        status = Kartenleser.MFRC522_Auth(Kartenleser.PICC_AUTHENT1A, 8, key, uid)
+        print("\n"+str(status))
         # Check if authenticated
-        #if status == Kartenleser.MI_OK:
-        # Read block 8
-    data = Kartenleser.MFRC522_Read(8)
-    print (data)
-    if data[:9] == authcode:
-        sample_func("\n"+str(data))
+        if status == Kartenleser.MI_OK:
+            # Read block 8
+            data = Kartenleser.MFRC522_Read(8)
+            print (data)
+            if data[:9] == authcode:
+                sample_func("\n"+str(data))
             #elif ...
