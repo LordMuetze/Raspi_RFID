c500b9fbe38e03db1df7bb12d36d984f00442dd6
Lord Muetze <valentin-herrmann@gmx.net>
2018-03-13 18:34:33 +0100

    Standard commit

--- RFID.py
+++ RFID.py
@@ -8,8 +8,8 @@ Kartenleser = MFRC522.MFRC522()
 
 def read():
         # Scan for cards    
-    (status,TagType) = Kartenleser.MFRC522_Request(Kartenleser.PICC_REQIDL)
-    print (TagType)
+    (status) = Kartenleser.MFRC522_Request(Kartenleser.PICC_REQIDL)
+
     # If a card is found
     if status == Kartenleser.MI_OK:
 
@@ -17,7 +17,7 @@ def read():
         (status,uid) = Kartenleser.MFRC522_Anticoll()
         print("\n\n")
         print(status)
-        print(TagType)
+
         print(uid)
         print("\n\n")
         return uid
