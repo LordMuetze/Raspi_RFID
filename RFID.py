243fb2a3de1b93dce553926770a7a04c056aa5fc
Lord Muetze <valentin-herrmann@gmx.net>
2018-03-13 19:00:29 +0100

    Standard commit

--- RFID.py
+++ RFID.py
@@ -31,8 +31,7 @@ def check(UID=[]):
         # Get the UID of the card
         (status,uid_raw) = Kartenleser.MFRC522_Anticoll()
         print(uid_raw)
-        uid = str(uid_raw[0])+str(uid_raw[1])+str(uid_raw[2])+str(uid_raw[3])+str(uid_raw[4])
-        uid = int(uid)
+        uid = int(str(uid_raw[0])+str(uid_raw[1])+str(uid_raw[2])+str(uid_raw[3])+str(uid_raw[4]))
         print("\n\n")
         print(uid)
         print("\n\n")
