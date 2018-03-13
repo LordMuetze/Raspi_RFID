6885779eda42d8c00b22b3783baad1c8addc6de4
Lord Muetze <valentin-herrmann@gmx.net>
2018-03-13 18:58:27 +0100

    Standard commit

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ import RFID
 from time import sleep
 
 while True:
-    zugang = RFID.check([128234125162181])
+    zugang = RFID.check(UID=[128234125162181])
     if zugang == True:
         print("Herein spaziert!")
     elif zugang == False:
