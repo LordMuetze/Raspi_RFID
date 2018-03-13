f1072d86853fc16e59b2f554f37aa3c05f051f97
Lord Muetze <valentin-herrmann@gmx.net>
2018-03-13 18:31:16 +0100

    Standard commit

--- RFID.py
+++ RFID.py
@@ -6,7 +6,7 @@ import MFRC522
 
 Kartenleser = MFRC522.MFRC522()
 
-def check(authcode):
+def read(authcode):
         # Scan for cards    
     (status,TagType) = Kartenleser.MFRC522_Request(Kartenleser.PICC_REQIDL)
 
@@ -16,19 +16,4 @@ def check(authcode):
         # Get the UID of the card
         (status,uid) = Kartenleser.MFRC522_Anticoll()
         print(uid)
-        # This is the default key for authentication
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        # Select the scanned tag
-        Kartenleser.MFRC522_SelectTag(uid)
-        # Authenticate
-        status = Kartenleser.MFRC522_Auth(Kartenleser.PICC_AUTHENT1A, 8, key, uid)
-        print("\n"+str(status))
-        # Check if authenticated
-        if status == Kartenleser.MI_OK:
-            # Read block 8
-            data = Kartenleser.MFRC522_Read(8)
-            print (data)
-            if data[:9] == authcode:
-                return True
-            else:
-                return False
+        return uid
