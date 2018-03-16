83040b0bee6b81ce0b8c18e583f5b617dd73adbe
LordMuetze <valentin-herrmann@gmx.net>
2018-03-16 16:35:52 +0100

    HI

--- 7Segment.py
+++ 7Segment.py
@@ -45,7 +45,7 @@ def an(nummer,anzeige=1):
     print(digits[anzeige-1])
     GPIO.output(digits[anzeige-1],GPIO.LOW)
     exec("for index in liste_"+str(nummer)+": real_index = index-1; GPIO.output(segments[real_index],GPIO.HIGH)")
-    GPIO.output(digits[anzeige-1],GPIO.HIGH)        
+    #GPIO.output(digits[anzeige-1],GPIO.HIGH)        
 
 def aus():
     global segments
